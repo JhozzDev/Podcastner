48a49485ba2f804392387e877b7aa6f4ed7f2492
JhozzDev <jhossanderhiciano@example.com>
2026-07-11 15:44:27 -0400

    Setup Interfaz

--- Podcastner/MainWindow.xaml.cs
+++ Podcastner/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Podcastner.Models;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,17 +9,44 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+namespace Podcastner;
 
-namespace Podcastner
+
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+
+
+        List<Podcast> podcasts =
+        [
+            new Podcast
+            {
+                Title = "6 Minute English",
+                Author = "BBC Learning English",
+                Description = "Improve your vocabulary with short lessons."
+            },
+
+
+            new Podcast
+            {
+                Title = "The English We Speak",
+                Author = "BBC Learning English",
+                Description = "Learn everyday English expressions."
+            },
+
+
+            new Podcast
+            {
+                Title = "All Ears English",
+                Author = "AEE",
+                Description = "English conversation practice."
+            }
+        ];
+
+
+        PodcastList.ItemsSource = podcasts;
     }
 }
---
+++ Podcastner/Models/Podcast.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Podcastner.Models
+{
+    public class Podcast
+    {
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string Description { get; set; }
+
+        public string AudioPath { get; set; }
+    }
+}
