ac0851e127ce96d7f45e752bca8bdebcd3b72bfa
JhozzDev <jhossanderhiciano@example.com>
2026-07-11 17:10:37 -0400

    Button styles Play and Stop function

--- Podcastner/MainWindow.xaml.cs
+++ Podcastner/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Podcastner.Models;
+using System.Numerics;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,15 +11,21 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 namespace Podcastner;
+using System.Windows.Threading;
+
 
 
 public partial class MainWindow : Window
 {
 
+    MediaPlayer player = new MediaPlayer();
+    DispatcherTimer timer = new DispatcherTimer();
     public MainWindow()
     {
         InitializeComponent();
 
+        timer.Interval = TimeSpan.FromSeconds(1);
+        timer.Tick += Timer_Tick;
 
         List<Podcast> podcasts =
         [
@@ -26,7 +33,8 @@ public partial class MainWindow : Window
             {
                 Title = "6 Minute English",
                 Author = "BBC Learning English",
-                Description = "Improve your vocabulary with short lessons."
+                Description = "Improve your vocabulary with short lessons.",
+                AudioPath = "C:\\Users\\Cristian hiciano\\source\\repos\\Podcastner\\Podcastner\\Resources\\Audio\\You.mp3"
             },
 
 
@@ -49,4 +57,42 @@ public partial class MainWindow : Window
 
         PodcastList.ItemsSource = podcasts;
     }
+
+    private void Play_Click(object sender, RoutedEventArgs e)
+    {
+        if (PodcastList.SelectedItem is Podcast podcast)
+        {
+            player.Open(new Uri(
+                podcast.AudioPath,
+                UriKind.Relative));
+
+
+            player.Play();
+
+
+            timer.Start();
+        }
+    }
+
+    private void Stop_Click(object sender, RoutedEventArgs e)
+    {
+        player.Stop();
+
+        timer.Stop();
+
+        TimeText.Text = "00:00 / 00:00";
+    }
+    private void Timer_Tick(object sender, EventArgs e)
+    {
+        if (player.NaturalDuration.HasTimeSpan)
+        {
+            TimeSpan actual = player.Position;
+            TimeSpan total = player.NaturalDuration.TimeSpan;
+
+
+            TimeText.Text =
+                $"{actual:mm\\:ss} / {total:mm\\:ss}";
+        }
+    }
+
 }
